40454689d778e194d46b57fec5e00066d002d2e6
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-06-01 20:56:09 +0300

    Stuck on how make divs auto fill the container

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 'use strict';
 
 const container = document.querySelector('.container');
+let amount = 16;
+for (let i = 0; i < amount * amount; i++) {
+	const cell = document.createElement('div');
+	cell.classList.add('brick');
+	container.appendChild(cell);
+}
