392ee9cdd25ea37941127e4f76b97ed9c05441f8
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-06-05 21:29:07 +0300

    add hovering effect

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ container.style.gridTemplateColumns = 'repeat(16, 1fr)';
 container.style.gridTemplateRows = 'repeat(16, 1fr)';
 let amount = 16;
 let banger = 1;
+const changeColor = e => {
+	e.target.classList.add('brick');
+};
+
 for (let i = 0; i < amount * amount; i++) {
 	const cell = document.createElement('div');
-	cell.classList.add('brick');
 	cell.classList.add(banger++);
 	container.insertAdjacentElement('beforeend', cell);
+	cell.addEventListener('mouseover', changeColor);
 }
