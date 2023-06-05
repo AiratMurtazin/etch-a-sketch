107ffe93bc37356762dce555b77f20535265109c
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-06-05 21:49:01 +0300

    Make it all inside a nice function

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 'use strict';
 
 const container = document.querySelector('.container');
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-container.style.gridTemplateRows = 'repeat(16, 1fr)';
-let amount = 16;
+const btn = document.querySelector('.btn');
+
 let banger = 1;
+
 const changeColor = e => {
 	e.target.classList.add('brick');
 };
 
-for (let i = 0; i < amount * amount; i++) {
-	const cell = document.createElement('div');
-	cell.classList.add(banger++);
-	container.insertAdjacentElement('beforeend', cell);
-	cell.addEventListener('mouseover', changeColor);
-}
+const populateCells = size => {
+	container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+	for (let i = 0; i < size * size; i++) {
+		const cell = document.createElement('div');
+		cell.classList.add(banger++);
+		container.insertAdjacentElement('beforeend', cell);
+		cell.addEventListener('mouseover', changeColor);
+	}
+};
+
+populateCells(16);
+
+btn.addEventListener('click', () => {
+	let answer = prompt('Choose how many cells you need');
+	numOfCells = answer;
+});
