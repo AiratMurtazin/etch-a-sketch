05fb847a8f570b5cb117ee49b2e1bfad087cab26
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-06-05 21:25:56 +0300

    make a respons container for divs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const container = document.querySelector('.container');
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+container.style.gridTemplateRows = 'repeat(16, 1fr)';
 let amount = 16;
+let banger = 1;
 for (let i = 0; i < amount * amount; i++) {
 	const cell = document.createElement('div');
 	cell.classList.add('brick');
-	container.appendChild(cell);
+	cell.classList.add(banger++);
+	container.insertAdjacentElement('beforeend', cell);
 }
